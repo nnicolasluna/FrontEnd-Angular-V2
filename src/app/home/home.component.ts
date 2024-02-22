9daf37b504187dbb8808c10428056695c025ac72
Joel Luna <nnicolasluna@gmail.com>
2024-02-22 12:59:12 -0400

    cambios en el sidenav

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,13 +8,31 @@ import { Component } from '@angular/core';
 
 })
 export class HomeComponent {
-  panelOpenState = false;
-  sidebarVisible = true; 
+  
+  sidebarVisible = true;
+  buttons = [
+    { name: 'Personas', icon: 'person', link:'personlist' },
+    { name: 'Usuarios', icon: '3p', link:'userlist' },
+    { name: 'Roles', icon: 'group', link:'rolelist' },
+    { name: 'Menus', icon: 'menu', link:'menulist' },
+    { name: 'Comandos', icon: 'terminal', link:'comandolist' },
+    { name: 'Subsistema', icon: 'badge', link:'subsistemalist' },
+    { name: 'Tipos Documentos', icon: 'badge', link:'tipodocumentolist' },
+    { name: 'Genero', icon: 'male', link:'genero-list' },
+    { name: 'Ocupación', icon: 'work', link:'ocupacion-list' },
+    { name: 'Estado Civil', icon: 'wc', link:'estado-civil-list' },
+
+
+  ];
   toggleSidebar() {
     this.sidebarVisible = !this.sidebarVisible;
   }
-  sidenav(){
-    this.sidebarVisible=false
+  sidenav() {
+    this.sidebarVisible = false
   }
-  
+  admin = false;
+  Admi() {
+    this.admin = !this.admin;
+  }
+
 }
