b0117d4b18f7dd3beb2337ddb0a13818333832f6
Joel Nicolas Luna <nnicolasluna@gmail.com>
2024-02-27 23:23:18 -0400

    arreglos menores

--- cypress/e2e/Identidad-Seguridad/Comando/comando-create.spect.cy.ts
+++ cypress/e2e/Identidad-Seguridad/Comando/comando-create.spect.cy.ts
@@ -1,12 +1,6 @@
 describe('Comando template', () => {
     it('Crear Comando', () => {
-        cy.visit('http://localhost:4200/home')
-        cy.url().should('include', 'home');
-        cy.get('[data-cy="Administracion"]').click();
-        cy.get('[data-cy="comandoslist"]').click();
-        cy.url().should('include', 'home/comandolist');
-        cy.get('[data-cy="crearcomando"]').click();
-        cy.url().should('include', 'home/comandocreate');
+        cy.visit('http://localhost:4200/home/comandocreate')
         cy.get('[formControlName="nombre"]').type('Eliminar')
         cy.get('[formControlName="descripcion"]').type('Elimar')
         cy.get('[formControlName="estado"]').select('Activado');
@@ -16,3 +10,5 @@ describe('Comando template', () => {
        
     })
 })
+
+
--- cypress/e2e/Identidad-Seguridad/Comando/comando-edit.spect.cy.ts
+++ cypress/e2e/Identidad-Seguridad/Comando/comando-edit.spect.cy.ts
@@ -1,14 +1,7 @@
 describe('Comando template', () => {
     it('Crear Comando', () => {
-        cy.visit('http://localhost:4200/home')
-        cy.url().should('include', 'home');
-        cy.get('[data-cy="Administracion"]').click();
-        cy.get('[data-cy="comandoslist"]').click();
-        cy.url().should('include', 'home/comandolist');
-        cy.get('[data-cy="editarcomando"]').eq(1).click();
-        cy.url().should('include', 'home/comandoedit');
-  
-        cy.get('[formControlName="uuid"]').select('CajeroCajero GOD');
+        cy.visit('http://localhost:4200/home/comandolist')
+        cy.get('[data-cy="editar"]').eq(0).click();
         cy.get('[data-cy="editarcomando"]').click();
         cy.url().should('include', 'home/comandolist');
        
