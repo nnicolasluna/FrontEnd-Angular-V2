cf0b2aadd29b0494800b9055a1cca77c4fee765d
Joel Nicolas Luna <nnicolasluna@gmail.com>
2024-03-04 23:31:57 -0400

    cambios en el footer

--- src/app/home/Documento/documento-create/documentocreate.component.ts
+++ src/app/home/Documento/documento-create/documentocreate.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DocumentoService } from '../documento-service/documento.service';
 import { documento } from '../documento-model/documento';
 import { TipoDocumentoService } from '../../tipoDocumento/tipo-documento-service/tipo-documento.service';
+import { MatTabGroup } from '@angular/material/tabs';
+import { MatTab } from '@angular/material/tabs';
+
+interface CustomMatTab extends MatTab {
+  id: string;
+}
 @Component({
   selector: 'app-documentocreate',
   templateUrl: './documentocreate.component.html',
@@ -13,6 +19,8 @@ export class DocumentocreateComponent {
   uuid!: any;
   tipodocuments: any;
   datos: any[] = [];
+  private url='/home/personprofile'
+  @ViewChild('tabGroup') tabGroup?: MatTabGroup;
   personaFormGroup = new FormGroup(
     {
       uuid: new FormControl(''),
@@ -43,17 +51,24 @@ export class DocumentocreateComponent {
     private router: Router,
     private documentoService: DocumentoService,
     private route: ActivatedRoute,
-    private tipoDocumentoService: TipoDocumentoService) { }
+    private tipoDocumentoService: TipoDocumentoService,
+    private cdr: ChangeDetectorRef
+    ) { }
 
   ngOnInit() {
     this.gettipoDoc();
     this.uuid = this.route.snapshot.paramMap.get('id');
   }
+  onSelectChange(event: any) {
 
+    this.cdr.detectChanges();
+  }
   create() {
     if (this.formGroup.valid) {
 
       this.uuid = this.route.snapshot.paramMap.get('id');
+
+
       this.personaFormGroup.value.uuid = this.uuid;
       this.formGroup.value.personas = this.personaFormGroup.value;
       console.log(this.formGroup.value)
@@ -61,14 +76,10 @@ export class DocumentocreateComponent {
       this.documentoService.create(this.formGroup.value as documento).subscribe({
 
         next: (userData: any) => {
-          if (userData) {
-            this.router.navigate(['/home/personprofile/', id]);
 
+            this.router.navigate(['/home/personprofile/', id])
             this.formGroup.reset();
-          }
-          else {
-            alert("Datos Incorrectos, Verifique sus datos");
-          }
+      
         },
       });
     }
@@ -76,12 +87,20 @@ export class DocumentocreateComponent {
       this.formGroup.markAllAsTouched();
     }
   }
-
+  volverA(identificador: string) {
+    if (this.tabGroup) {
+      const tab = (this.tabGroup._tabs.find(t => (t as any).id === identificador) as any);
+      if (tab) {
+        this.tabGroup.selectedIndex = this.tabGroup._tabs.toArray().indexOf(tab);
+      }
+    }
+  }
   gettipoDoc() {
     this.tipoDocumentoService.getDocuments().subscribe(
       {
         next: (data) => {
           this.datos = data;
+          console.log(data)
         },
         error: err =>{
           console.log('no se pueden obteneder los datos de tipo de documento')
--- src/app/home/form/form-footer/form-footer.component.ts
+++ src/app/home/form/form-footer/form-footer.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class FormFooterComponent {
 
   @Output() saveClicked = new EventEmitter<void>();
-  @Input() routerLink!: string;
+  @Input() regreso!: string;
 
   save() {
     this.saveClicked.emit();
